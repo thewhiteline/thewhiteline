dfca76639e63291af3ed72a72eadd7c7294f3ed1
Ravern Koh <ravernkoh@gmail.com>
2018-09-06 11:07:06 +0800

    Add ordered-imports rule to TSLint and fix errors

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
-import request from "supertest";
-import { Test } from "@nestjs/testing";
-import { AppModule } from "./../src/app/app.module";
 import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+
+import { AppModule } from "./../src/app/app.module";
 
 describe("AppController (e2e)", () => {
   let app: INestApplication;
