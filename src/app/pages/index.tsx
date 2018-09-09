db4ae426725e38eaa21a6c77a5fef4ef7af26b72
Ravern Koh <ravernkoh@gmail.com>
2018-09-09 14:32:55 +0800

    Change to explicit Fragment usage instead of special JSX

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Component } from "react";
+import { Component, Fragment } from "react";
 
 export default class extends Component {
   public render() {
     return (
-      <>
+      <Fragment>
         <h1>The White Line</h1>
         <h3>Learn because you want to</h3>
-      </>
+      </Fragment>
     );
   }
 }
