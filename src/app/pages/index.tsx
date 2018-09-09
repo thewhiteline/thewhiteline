e4d3119cff34528fa64cecba692cfbf7a6867ebc
Ravern Koh <ravernkoh@gmail.com>
2018-09-09 11:37:41 +0800

    Add documentation about development setup

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -2,6 +2,11 @@ import { Component } from "react";
 
 export default class extends Component {
   render() {
-    return <h1>The White Line</h1>;
+    return (
+      <>
+        <h1>The White Line</h1>
+        <h3>Learn because you want to</h3>
+      </>
+    );
   }
 }
