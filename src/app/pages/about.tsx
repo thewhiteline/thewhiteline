c5204d9bebaaccb2a8792e3cf6438e63cebbf55d
Ravern Koh <ravernkoh@gmail.com>
2018-09-21 13:16:54 +0800

    Create about page

---
+++ src/app/pages/about.tsx
@@ -0,0 +1,11 @@
+import { Component, Fragment } from "react";
+
+export default class extends Component {
+  public render() {
+    return (
+      <Fragment>
+        <h1>About</h1>
+      </Fragment>
+    );
+  }
+}
